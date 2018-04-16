23922f0f7b10fecebceb5d09f868fa07607d5586
BenJakob <bennyjakob@me.com>
2018-04-16 16:25:53 +0200

    equipment

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,22 @@ $("#login-btn").click(function() {
 $("#register-btn").click(function() {
     window.location.href = 'register.html';
 })
+
+$('#recipeCarousel').carousel({
+  interval: 10000
+})
+
+$('.carousel .carousel-item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+    }
+    else {
+      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+});
