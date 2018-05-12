a99df33e6f114779501850fb76c5b4604d8a3ba9
BenJakob <bennyjakob@me.com>
2018-05-12 10:37:47 +0200

    executing templates [work in progress]

--- app/controller/admin.go
+++ app/controller/admin.go
@@ -1,8 +1,20 @@
 package routes
 
-import "io"
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+
+	"borgdir.media/app/model"
+)
 
 func Admin(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Admin")
+
+	// t := template.Must(template.ParseFiles("template/admin.html"))
+
+	t := template.Must(template.ParseFiles("template/layout.gohtml"))
+	headerData := model.Header{Title: "Admin", Css: []string{}}
+	navbarItems := model.NavItem{Title: "Equipment", Link: "admin-equipment.html"}
+
+	t.Execute(w, headerData)
+
 }
