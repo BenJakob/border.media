b9d97afe14fe2608dff80803aa0381d61664a749
BenJakob <bennyjakob@me.com>
2018-05-22 18:48:52 +0200

    added serveResource for css, img and js

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "net/http"
-import "borgdir.media/app/controller"
+import (
+	"bufio"
+	"net/http"
+	"os"
+	"strings"
+
+	"borgdir.media/app/controller"
+)
 
 func main() {
 
@@ -17,6 +23,40 @@ func main() {
 	http.HandleFunc("/my-equipment", routes.AdminsClients)
 	http.HandleFunc("/profil", routes.AdminEditClients)
 	http.HandleFunc("/register", routes.AdminEquipment)
+
+	http.HandleFunc("/img/", serveResource)
+	http.HandleFunc("/css/", serveResource)
+	http.HandleFunc("/scripts/", serveResource)
+
 	http.ListenAndServe(":8080", nil)
 
+}
+
+func serveResource(w http.ResponseWriter, req *http.Request) {
+	path := "static" + req.URL.Path
+	var contentType string
+
+	if strings.HasSuffix(path, ".css") {
+		contentType = "text/css; charset=utf-8"
+	} else if strings.HasSuffix(path, ".js") {
+		contentType = "application/javascript; charset=utf-8"
+	} else if strings.HasSuffix(path, ".png") {
+		contentType = "image/png; charset=utf-8"
+	} else if strings.HasSuffix(path, ".jpg") {
+		contentType = "image/jpg; charset=utf-8"
+	} else if strings.HasSuffix(path, ".svg") {
+		contentType = "image/svg+xml; charset=utf-8"
+	} else {
+		contentType = "text/plain; charset=utf-8"
+	}
+
+	f, err := os.Open(path)
+	if err == nil {
+		defer f.Close()
+		w.Header().Add("Content-Type", contentType)
+		br := bufio.NewReader(f)
+		br.WriteTo(w)
+	} else {
+		w.WriteHeader(404)
+	}
 }
