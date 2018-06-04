bcd1493f5587619c539c49152f847466d98472de
BenJakob <bennyjakob@me.com>
2018-06-04 18:26:10 +0200

    fixed search and filter bug

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
     $("#search-box").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-
-        $(".filter-element").filter(function() {
-            $(this).toggle($(this).attr("data-name").toLowerCase().indexOf(value) > -1)
-        });
+        // var value = $(this).val().toLowerCase();
+        //
+        // $(".filter-element").filter(function() {
+        //     $(this).toggle($(this).attr("data-name").toLowerCase().indexOf(value) > -1)
+        // });
+        filterRows()
     });
 
     $("#select-sortorder").on("change", function() {
@@ -18,15 +19,41 @@ $(document).ready(function(){
     });
 
     $("#select-category").on("change", function() {
-        var value = $(this).val()
-        if (value == "all") {
+        // var value = $(this).val()
+        // if (value == "all") {
+        //     $(".filter-element").filter(function() {
+        //         $(this).toggle(true)
+        //     });
+        // } else {
+        //     $(".filter-element").filter(function() {
+        //         $(this).toggle($(this).attr("data-category").indexOf(value) > -1)
+        //     });
+        // }
+        filterRows()
+    });
+
+    function filterRows() {
+        const searchValue = $("#search-box").val().toLowerCase();
+        const categoryValue = $("#select-category").val()
+
+        if (categoryValue == "all" && searchValue == "") {
             $(".filter-element").filter(function() {
                 $(this).toggle(true)
             });
+        } else if (categoryValue == "all" && searchValue != "") {
+            $(".filter-element").filter(function() {
+                $(this).toggle($(this).attr("data-name").toLowerCase().indexOf(searchValue) > -1)
+            });
+        } else if (categoryValue != "all" && searchValue == "") {
+            $(".filter-element").filter(function() {
+                $(this).toggle($(this).attr("data-category").indexOf(categoryValue) > -1)
+            });
         } else {
             $(".filter-element").filter(function() {
-                $(this).toggle($(this).attr("data-category").indexOf(value) > -1)
+                const searchResult = $(this).attr("data-name").toLowerCase().indexOf(searchValue) > -1
+                const categoryResult = $(this).attr("data-category").indexOf(categoryValue) > -1
+                $(this).toggle(searchResult && categoryResult)
             });
         }
-    });
+    }
 });
